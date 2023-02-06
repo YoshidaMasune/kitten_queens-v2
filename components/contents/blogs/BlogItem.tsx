3a8c07ea94067f325bdf7371802f571209f70915
devmonkna <samaneranateechai@gmail.com>
2023-02-06 14:47:04 +0700

    update blogs

---
+++ components/contents/blogs/BlogItem.tsx
@@ -0,0 +1,37 @@
+import { blog } from '@/data/blogs';
+import Image from 'next/image';
+import React from 'react';
+
+interface blogItem {
+  blog: blog;
+}
+
+function BlogItem({ blog }: blogItem) {
+  return (
+    <div className="grid grid-cols-[1fr_4fr] shadow-md rounded-md overflow-hidden">
+      <div>
+        <Image
+          src={`/cats/persian/06.png`}
+          width={400}
+          height={400}
+          priority
+          alt="cats"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* content */}
+      <div>
+        <article className="flex flex-col justify-between h-auto">
+          <h3 className="text-lg">{blog.head}</h3>
+          <p className="line-clamp-3 indent-5">
+            {blog.subtitle || blog.contents}
+          </p>
+          <button className="btn-primary self-end ">อ่านต่อ</button>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+export default BlogItem;
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,33 +1,47 @@
+import BlogItem from '@/components/contents/blogs/blogItem';
 import Layout from '@/components/layout/Layout';
+import { blog } from '@/data/blogs';
 import getData from '@/data/fetchdata/getData';
 import Head from 'next/head';
 import React, { useState, useEffect } from 'react';
 
 function index() {
-  const [blogs, setBlogs] = useState<Array<any>>();
+  const [blogs, setBlogs] = useState<Array<blog | any>>([]);
   useEffect(() => {
     const data = getData('blogs');
     setBlogs(data);
   }, []);
+
   return (
     <div>
       <Head>
         <title>blogs</title>
       </Head>
       <Layout>
-        <div>
+        <div className="grid grid-cols-1 lg:grid-cols-[3fr_1fr] container lg:w-3/5 mx-auto">
           {/* header */}
-          <section>
-            <article>
-              <h1>บทความ</h1>
-              <h2>Cat{"'"}s blos</h2>
-            </article>
-          </section>
+          <div>
+            <section>
+              <article className="text-4xl">
+                <h1 className="text-sky-500">บทความ</h1>
+                <h2 className="text-yellow-500 indent-3">Cat{"'"}s blos</h2>
+              </article>
+            </section>
 
-          {/* content */}
-          <section></section>
+            {/* content */}
+            <section>
+              {blogs.length === 0 ? null : (
+                <div className="grid grid-flow-row mx-auto gap-y-10 auto-rows-fr">
+                  {blogs.map((blog, index) => {
+                    return <BlogItem key={index} blog={blog} />;
+                  })}
+                </div>
+              )}
+            </section>
+          </div>
+
           {/* aside */}
-          <section></section>
+          <section className="hidden lg:block"></section>
         </div>
       </Layout>
     </div>
