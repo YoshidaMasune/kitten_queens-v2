efb2213f7e0a3ee7cea818a14349f724056d0705
natee <samaneranateechai@gmail.com>
2023-02-07 14:31:17 +0700

    update slide ad video slide in index page

--- components/contents/vides/CatsVidios.tsx
+++ components/contents/vides/CatsVidios.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const linkes = [
   'https://www.youtube.com/embed/x8fVvhMWP80',
   'https://www.youtube.com/embed/9YeXnYg2_1Q',
   'https://www.youtube.com/embed/FQjiexhx4Uc',
-  //   'https://www.youtube.com/embed/7wj_kfKXxMg',
-  //   'https://www.youtube.com/embed/3NygEITEncc',
+  'https://www.youtube.com/embed/7wj_kfKXxMg',
+  'https://www.youtube.com/embed/3NygEITEncc',
 ];
 
 function CatsVidios() {
+  const slidersetting = {
+    focusOnSelect: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
   return (
     <div className="container mx-auto space-y-10 py-20">
       <div>
@@ -17,10 +31,12 @@ function CatsVidios() {
           <h2 className="text-yellow-600">ที่น่าสนใจ</h2>
         </article>
       </div>
-      <div className="grid grid-flow-col justify-items-center">
-        {linkes.map((vi, index) => (
-          <Video link={vi} key={index} />
-        ))}
+      <div className="">
+        <Slider {...slidersetting}>
+          {linkes.map((vi, index) => (
+            <Video link={vi} key={index} />
+          ))}
+        </Slider>
       </div>
     </div>
   );
@@ -33,7 +49,7 @@ interface video {
 const Video = ({ link }: video) => {
   return (
     <iframe
-      className="rounded-lg"
+      className="rounded-lg w-11/12 mx-auto"
       width="360"
       height="215"
       src={link}
